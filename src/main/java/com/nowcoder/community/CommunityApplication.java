49504dc3c6043ac9489415e024617fe819f62f2f
xucan <1426955740@qq.com>
2022-03-09 20:29:01 +0800

    day2 发送邮件  开发注册功能2

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan({"com.nowcoder.community.dao"})
-
 public class CommunityApplication {
 
 	public static void main(String[] args) {
